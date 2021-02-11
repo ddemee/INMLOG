e1515504d00f9b5f2fbcf9345b3b3b68fc00aa86
Dima Demetrashvili <74451049+ddemee@users.noreply.github.com>
2021-02-11 14:03:13 +0400

    FIRTS PG

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Header Scroll ფუნქცია
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 90) {
+        $(".header").addClass("scrolled");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".header").removeClass("scrolled");
+    }
+});
--- js/mapdata.js
+++ js/mapdata.js
@@ -9,7 +9,7 @@ var simplemaps_worldmap_mapdata={
     
 		//State defaults
 		state_description: "State description",
-    state_color: "#88A4BC",
+    state_color: "#11698e",
     state_hover_color: "#3B729F",
     state_url: "",
     border_size: 1.5,
